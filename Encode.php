e2c2b773828ce006822378dfaf913dc0a3be1dff
Muhammad Rafli <44805762+RabbitCL4Y@users.noreply.github.com>
2019-01-21 22:32:34 +0800

    Create Encode.php

---
+++ Encode.php
@@ -0,0 +1,60 @@
+<?php
+echo "###########################################################
+        Author By   : MR.CL4Y
+     Contact WA Me  : 085752481601
+        My Team     : Rabbit Cyber Team
+        My Team     : 2Easy4Hack Teamm
+        My Team     : KALIMANTAN SQUAD
+        My Github   : https://github.com/RabbitCL4Y
+        My Facebook : Mhmmd Rafli
+        My Instagram: muhammadrafli_337
+                ^_^SCRIPTKIDDIES^_^
+###########################################################\n\n";
+        // Print out main menu..
+    echo "=======================\n";
+    echo "Tools Encode\n";
+    echo "=======================\n";
+    echo "Select an option..\n\n";
+    echo "    1) Encode Md5\n";
+    echo "    2) Encode Base64\n";
+    echo "    3) Encode Sha1\n";
+    echo "    4) Encode Md4\n";
+    echo "    5) Encode Semua type\n";
+    echo "    x) Exit\n";
+    echo "Pilih Type Encode : ";
+    switch(trim(fgets(STDIN,256)))
+        {
+            case 1:
+                echo "Masukkan Kata Yang Akan di Encode : ";
+                $kata = trim(fgets(STDIN,256));
+                echo "Encode Md5 :",md5($kata),"\n";
+                exit();
+            case 2:
+                echo "Masukkan Kata Yang Akan di Encode : ";
+                $kata = trim(fgets(STDIN));
+                echo "Encode Base64 :",base64_encode($kata),"\n";
+                exit();
+            case 3:
+                echo "Masukkan Kata Yang Akan di Encode : ";
+                $kata = trim(fgets(STDIN));
+                echo "Encode Base64 :",sha1($kata),"\n";
+                exit();
+            case 4:
+                echo "Masukkan Kata Yang Akan di Encode : ";
+                $kata = trim(fgets(STDIN));
+                echo "Encode Md4 :",crypt('md4',$kata);
+                exit();
+            case 5:
+                echo "Masukan Kata Yang Akan di Encode : ";
+                $kata = trim(fgets(STDIN));
+                foreach (hash_algos() as $v) {
+                $r = hash($v, $kata, false);
+                printf("%-12s %3d %s\n", $v, strlen($r), $r);
+                }
+                exit();
+            case "x":
+                exit();
+        }
+    // Close standard in..
+    fclose(STDIN);
+?>
